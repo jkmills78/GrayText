bcf3690f48a725b5e4adb99da6d2663b553f27e8
jkmills78 <jkmills78@outlook.com>
2016-04-02 11:32:14 -0400

    Add project files.

---
+++ Gray Text/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gray_Text
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TextCheck textCheck;
+        private StringBuilder stringBuilder;
+        private string lastWord;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            textCheck = new TextCheck();
+            lastWord = string.Empty;
+            stringBuilder = new StringBuilder();
+            textBox.Focus();
+        }
+
+        private void onTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textCheck.IsDelimiter(textBox))
+            {
+                if (textCheck.BadWord(lastWord))
+                {
+                    e.Handled = true;
+                    textBox.Text = stringBuilder.ToString();
+                    textBox.CaretIndex = textBox.Text.Length;
+                }
+                else
+                {
+                    stringBuilder.Clear();
+                    stringBuilder.Append(textBox.Text);
+                }
+
+                lastWord = string.Empty;
+            }
+            else
+            {
+                lastWord += textBox.ToString().LastOrDefault();
+            }
+        }
+    }
+}
---
+++ Gray Text/TextCheck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Gray_Text
+{
+    public class TextCheck
+    {
+        private List<string> blackList;
+
+        public TextCheck()
+        {
+            blackList = new List<string>();
+
+            // Initliaize black list
+            blackList.Add("the");
+            blackList.Add("and");
+            blackList.Add("a");
+            blackList.Add("of");
+            blackList.Add("MySuperLongTextTestString");
+        }
+
+        public bool BadWord(string str)
+        {
+            if (blackList.Contains(str.ToLower()))
+                return true;
+            else
+                return false;
+        }
+
+        public bool IsDelimiter(TextBox textBox)
+        {
+            char chr = textBox.ToString().LastOrDefault();
+            if (Char.IsPunctuation(chr) || chr == ' ')
+                return true;
+            else
+                return false;
+        }
+    }
+}
